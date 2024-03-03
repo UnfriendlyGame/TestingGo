2cbe0e9f53719cc5014689d1961f0c5189cb1952
Stanislav Melo <stanislav.melo@siemens-healthineers.com>
2024-03-03 21:08:37 +0100

    starting a calculator app

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
 	fmt.Println("Hello World")
+	numOfArgs := len(os.Args)
+	if numOfArgs != 1 && numOfArgs != 3 {
+		fmt.Println("Wrong Number Of ARGUEMNTS")
+		os.Exit(-1)
+	}
+	if numOfArgs == 1 {
+		fmt.Println("enter arguments")
+	}
+	if numOfArgs == 3 {
+		result := strconv.Atoi(os.Args[1]) + strconv.Atoi(os.Args[2])
+		fmt.Println("%v + %v = %v", os.Args[1], os.Args[2], result)
+	}
 }
